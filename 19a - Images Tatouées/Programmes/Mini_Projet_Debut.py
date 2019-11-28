34ca077b85b3ba8db56b170b753f1a9876b344d0
haroldao <harold86340@gmail.com>
2019-11-28 20:21:15 +0100

    Ajout de commentaires + Modifications du code

--- 19a - Images Tatouées/Programmes/Mini_Projet_Debut.py
+++ 19a - Images Tatouées/Programmes/Mini_Projet_Debut.py
@@ -1,20 +1,32 @@
 ﻿from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-
+#https://stackoverflow.com/questions/47123649/pil-draw-trans<parent-text-on-top-of-an-image
+# https://pillow.readthedocs.io/en/4.2.x/reference/ImageDraw.html#example-draw-partial-opacity-text
 
 
 def watermark_text(input_image_path, output_image_path, text, pos_text):
-    original_image=Image.open(input_image_path)
-    #original_image.putalpha(10)
-    drawing = ImageDraw.Draw(original_image)
-    
+    #Importation de l'image de fond
+    original_image=Image.open(input_image_path).convert("RGBA")
 
-    white_color=(255,255,255, 15)
+    #Nouvelle variation avec une image vide transparente
+    txt = Image.new('RGBA', original_image.size, (255,255,255,0))
+
+    #Avoir un contexte pour dessiner
+    drawing = ImageDraw.Draw(txt)
+
+    #Definition de la couleur
+    color=(0,0,0, 80)
+
+    #Importation d'une police (en local)
     font= ImageFont.truetype(".../Montserrat-Bold.ttf", 40)
-    drawing.text(pos_text, text, fill=white_color, font=font, align= "right")
-    original_image.show()
-    original_image.save(output_image_path)
+
+    #Dessin du texte avec plusieurs paramètres (Position, texte, police, alignement )
+    drawing.text(pos_text, text, fill=color, font=font, align= "right")
+
+    final_image = Image.alpha_composite(original_image, txt) 
+    final_image.show()
+    final_image.save(output_image_path)
 
 
 if __name__ == '__main__':
