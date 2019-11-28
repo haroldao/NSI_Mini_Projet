4e34bd5dadd465d96f5259d4fa1d7aa5f9a6507f
haroldao <harold86340@gmail.com>
2019-11-28 20:39:02 +0100

    Ajout de commentaires + Modifications mineures du code

--- 19a - Images Tatouées/Programmes/Mini_Projet_Evolution_2.py
+++ 19a - Images Tatouées/Programmes/Mini_Projet_Evolution_2.py
@@ -1,33 +1,33 @@
 #ICN Logo (Evolution 2)
-
 from PIL import Image
-def watermark_logo(input_image_path, output_image_path, pos_img):
-    original_image=Image.open(input_image_path)
-    watermark_logo=Image.open(output_image_path)
 
-    new_width =500
+def watermark_logo(input_image_path, input2_image_path, output_image_path, pos_img):
+    #Import des images
+    original_image=Image.open(input_image_path).convert("RGBA")
+    watermark_logo=Image.open(input2_image_path)
+
+    #Definitions de nouvelles variables pour redimensionner le logo NSI
+    new_width=50
     new_height=50
-    watermark_logo=watermark_logo.resize((new_width, new_height), Image.ANTIALIAS)
 
-    
+    #Redimension du Logo
+    watermark_logo=watermark_logo.resize((new_width, new_height))
 
+    #Nouvelle variation qui convient une image vide transparente
+    transparent=Image.new("RGBA", original_image.size , (0,0,0,0))
 
-    weight, height=original_image.size
-
-    transparent=Image.new("RGBA",(weight, height) , (0,0,0,0))
+    #Collage de l'image en fond
     transparent.paste(original_image)
-    transparent.paste(watermark_logo, pos_img, mask=watermark_logo)
 
+    #Collage du Watermak_Logo(NSI) et attribution du paramètre pos_img(Position)
+    transparent.paste(watermark_logo, pos_img)
+
+    #transparent = Image.alpha_composite(original_image, watermark_logo) 
 
     transparent.show()
     transparent.save(output_image_path)
 
-
-
-
-
-
-
 if __name__ == '__main__':
     img='../Ressources/cathedrale.jpg'
-    watermark_logo(img, "../Rendus/NSI_Logo.png", pos_img=(0,0))
+    img_2='../Ressources/NSI_Logo.png'
+    watermark_logo(img, img_2, "../Rendus/NSI_Logo.png", pos_img=(100,0))
