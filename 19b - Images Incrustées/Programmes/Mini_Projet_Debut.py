cb8b4e6df30068096fbb91879468f64a5fd244c8
haroldao <harold86340@gmail.com>
2019-11-29 09:40:26 +0100

    Modifications Mineures

--- 19b - Images Incrustées/Programmes/Mini_Projet_Debut.py
+++ 19b - Images Incrustées/Programmes/Mini_Projet_Debut.py
@@ -2,23 +2,29 @@ from PIL import Image
 #https://stackoverflow.com/questions/35531932/pil-remove-background-image-from-image
 
 def Incrustation (input_image_puth, input2_image_path, output_image_path):
-    #Import de l'image + Transformation de l'image en RGBA pour travailler avec la transparence (ALPHA)
+    #Import des images + Transformation de l'image en RGBA pour travailler avec la transparence (ALPHA)
     original_image=Image.open(input_image_puth).convert("RGBA")
     background_image=Image.open(input2_image_path).convert("RGBA")
-    #Lire les informations de l'image (les données des pixels)
     
+    #Lire les informations de l'image (les données des pixels)
     datas =original_image.getdata()
+
     # #Récuperation des dimensions de l'image 
     width, height = original_image.size
-    # transparent = Image.new("RGBA", (width, height))
+
+    #Définition d'une liste vide pour les datas
     newData=[]
-    for p in datas:
-        if p[0]== 0 and p[1]== 0 and p[2]== 255:
+
+    for item in datas:
+        if item[0]== 0 and item[1]== 0 and item[2]== 255:
             newData.append((255, 255, 255, 0))
         else:
-            newData.append(p)
-    
+            newData.append(item)
+
+    #Changement des datas de l'image (présentatrice)
     original_image.putdata(newData)
+
+    #Copie de la présentatrice sur la carte météo
     background_image.paste(original_image, (50,0), original_image)
     background_image.show()
 
@@ -26,12 +32,3 @@ if __name__ == '__main__':
     img="../Ressources/presentatrice meteo.bmp"
     img2="../Ressources/Carte meteo.bmp"
     Incrustation(img, img2, "../Rendus/Incrustation.png")
-
-# Im 1 = P1
-# im 2 = P2 (presentatrice)
-
-# si p1 est bleu
-# p=P1
-
-# sinon
-# p=p'1
