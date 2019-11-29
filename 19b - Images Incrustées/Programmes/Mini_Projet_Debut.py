fc995723d3ad16a68425502ce9154ed8d8bf27da
haroldao <harold86340@gmail.com>
2019-11-29 09:37:18 +0100

    Modifications Majeures du code

--- 19b - Images Incrustées/Programmes/Mini_Projet_Debut.py
+++ 19b - Images Incrustées/Programmes/Mini_Projet_Debut.py
@@ -1,29 +1,37 @@
 from PIL import Image 
 #https://stackoverflow.com/questions/35531932/pil-remove-background-image-from-image
 
-def RemoveBG (input_image_puth, output_image_path):
+def Incrustation (input_image_puth, input2_image_path, output_image_path):
     #Import de l'image + Transformation de l'image en RGBA pour travailler avec la transparence (ALPHA)
     original_image=Image.open(input_image_puth).convert("RGBA")
-
-    #Lire les informations de l'image
-    pixdata = original_image.load()
-
-    #Recuperation des dimensions de l'image 
+    background_image=Image.open(input2_image_path).convert("RGBA")
+    #Lire les informations de l'image (les données des pixels)
+    
+    datas =original_image.getdata()
+    # #Récuperation des dimensions de l'image 
     width, height = original_image.size
+    # transparent = Image.new("RGBA", (width, height))
+    newData=[]
+    for p in datas:
+        if p[0]== 0 and p[1]== 0 and p[2]== 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(p)
     
-    for y in range(height):
-        for x in range(width):
-            if pixdata[x, y] == (0, 0, 255, 255):
-                pixdata[x, y] = (0, 0, 0, 255)
-
-
-    
-    original_image.show()
-    original_image.save(output_image_path, "PNG")
+    original_image.putdata(newData)
+    background_image.paste(original_image, (50,0), original_image)
+    background_image.show()
 
 if __name__ == '__main__':
     img="../Ressources/presentatrice meteo.bmp"
-    RemoveBG(img, "../Rendus/RemoveBG.png")
+    img2="../Ressources/Carte meteo.bmp"
+    Incrustation(img, img2, "../Rendus/Incrustation.png")
 
-def Incrustation(bg_image):
-    bg_image=Image.open(bg_image)
+# Im 1 = P1
+# im 2 = P2 (presentatrice)
+
+# si p1 est bleu
+# p=P1
+
+# sinon
+# p=p'1
